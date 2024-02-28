ef7dbe36dc5229a81ea3138a75d7eac44dc0cbff
Coin Runner <coins@radiant.bitmark.co>
2024-02-28 10:28:12 +0000

    add mediantime to block info

--- src/rpcblockchain.cpp
+++ src/rpcblockchain.cpp
@@ -373,6 +373,7 @@ Object blockToJSON(const CBlock& block, const CBlockIndex* blockindex)
         txs.push_back(tx.GetHash().GetHex());
     result.push_back(Pair("tx", txs));
     result.push_back(Pair("time", block.GetBlockTime()));
+    result.push_back(Pair("mediantime", blockindex->GetMedianTimePast()));
     result.push_back(Pair("nonce", (uint64_t)block.nNonce));
     result.push_back(Pair("bits", HexBits(block.nBits)));
     result.push_back(Pair("difficulty", GetDifficulty(blockindex,algo,true,false)));
